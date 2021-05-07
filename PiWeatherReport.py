7e2abf01ec0659394f36748cdb6b65770806795f
multifeb13 <musicfair@gmail.com>
2021-05-07 15:03:24 +0900

    refactor: remove unused library

--- PiWeatherReport.py
+++ PiWeatherReport.py
@@ -3,10 +3,9 @@ import time
 from datetime import datetime
 
 #for OLED
-from luma.core.interface.serial import i2c, spi, pcf8574
-from luma.core.interface.parallel import bitbang_6800
+from luma.core.interface.serial import i2c
 from luma.core.render import canvas
-from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
+from luma.oled.device import ssd1306
 from PIL import Image, ImageFont, ImageDraw, ImageOps, ImageChops
 
 import os

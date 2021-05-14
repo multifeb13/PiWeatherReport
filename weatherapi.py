81ad09d4bbd31e26064e9eaf9461a268406137c9
hirotaka <musicfair@gmail.com>
2021-05-15 02:26:16 +0900

    fix: remove api key

--- weatherapi.py
+++ weatherapi.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "03308fa5a441cb0ddf1bfe85099e014d"
+API_KEY = "{API_KEY}"
 api = "https://api.openweathermap.org/data/2.5/onecall?lat=35.681236&lon=139.767125&units=metric&lang=ja&appid={key}"
 
 class weatherapi:

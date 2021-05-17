bf345386848744740e30914c4b520f8662c4de46
hirotaka <musicfair@gmail.com>
2021-05-17 15:20:38 +0900

    add: function of status code for 'weatherapi'
    status_code()

--- weatherapi.py
+++ weatherapi.py
@@ -3,6 +3,8 @@ import requests
 API_KEY = "{API_KEY}"
 api = "https://api.openweathermap.org/data/2.5/onecall?lat=35.681236&lon=139.767125&units=metric&lang=ja&appid={key}"
 
+m_status_code = 0
+
 class weatherapi:
 	def __init__(self):
 		pass
@@ -11,7 +13,15 @@ class weatherapi:
 		url = api.format(key = API_KEY)
 		#print(url)
 
-		return requests.get(url)
+		request = requests.get(url)
+		global m_status_code
+		m_status_code = request.status_code
+		#print(m_status_code)
+
+		return request
+
+	def status_code(self):
+		return m_status_code
 
 if __name__ == '__name__':
 	pass
